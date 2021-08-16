1fc4971478a6aff2165b694eb890cdd1251af8b1
samdychen <samdy.chen@visionwx.com>
2021-08-16 17:00:23 +0800

    【优化】抽离 appKey和appSecret到新的文件

--- android/src/main/java/com/sps/tuya_plugin/AppKey.java
+++ android/src/main/java/com/sps/tuya_plugin/AppKey.java
@@ -1,7 +1,7 @@
 public class AppKey {
     // dev: e597q33jgr4tnm3fgj83, gdau9an95apxug4hc8xm53aftnugr3g5
     // uat: pwn7wanmg3ka7qxs5hq7, 98g3rxhqd7gc739ypcay8n9mxrmsnycd
-    public static final KEY = "pwn7wanmg3ka7qxs5hq7";
-    public static final SECRET = "98g3rxhqd7gc739ypcay8n9mxrmsnycd";
-    public static final SCHEMA = "koneckonec";
+    public static final String KEY = "pwn7wanmg3ka7qxs5hq7";
+    public static final String SECRET = "98g3rxhqd7gc739ypcay8n9mxrmsnycd";
+    public static final String SCHEMA = "koneckonec";
 }
